10d6c6aa5097d3f51b471de46a1a5f023cc4968b
utkarsh29081997 <38398744+utkarsh29081997@users.noreply.github.com>
2022-05-31 17:30:22 +0530

    Update ArrangeAlterPositiveNegativeNumber.java

--- SeleniumPractice/BasicPrograms/src/com/BasciPrograms/ArrangeAlterPositiveNegativeNumber.java
+++ SeleniumPractice/BasicPrograms/src/com/BasciPrograms/ArrangeAlterPositiveNegativeNumber.java
@@ -11,6 +11,7 @@ public class ArrangeAlterPositiveNegativeNumber {
 		int countOfPositive = 0;
 		int countOfNegative = 0;
 
+		// Trying to find out the length of array to store positive and negative values in different array's
 		for (int i = 0; i <= a.length - 1; i++) {
 			if (a[i] > 0) {
 				countOfPositive++;
@@ -24,8 +25,11 @@ public class ArrangeAlterPositiveNegativeNumber {
 		int b[] = new int[countOfPositive];
 		int c[] = new int[countOfNegative];
 		int d[] = new int[a.length];
+		
+		// Segregating the arrays and filling up the arrays based on positive and negative values 
 		int countArrayNegative = 0;
 		int countArrayPositive = 0;
+		
 		for (int j = 0; j <= a.length - 1; j++) {
 			if (a[j] > 0) {
 				z = a[j];
@@ -40,6 +44,8 @@ public class ArrangeAlterPositiveNegativeNumber {
 
 		System.out.println(Arrays.toString(b));
 		System.out.println(Arrays.toString(c));
+		
+		// Filling up the new array with alternate values 
 		int count = 0;
 		for (int k = 0; k <= a.length - 1; k++) {
 			if (b.length == c.length) {
